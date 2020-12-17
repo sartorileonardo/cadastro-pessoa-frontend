d37d41d6dab39b76e66974e87d71b6f7254abc3d
leonardos <leonardos@koerich.com.br>
2020-12-17 00:23:07 -0300

    change labels html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { FlightModule } from './flight/flight.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NgModule } from '@angular/core';
@@ -10,16 +9,13 @@ import { APP_EXTRA_OPTIONS, APP_ROUTES } from './app.routes';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HotelModule } from './hotel/hotel.module';
 import { PessoaModule } from './pessoa/pessoa.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
-    FlightModule,
     RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS}),
-    HotelModule,
     PessoaModule,
   ],
   declarations: [
